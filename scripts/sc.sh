6d3332c51a1f2594ffecd9120615866da29af4d8
Jindrich Pilar <pilar.jindrich@gmail.com>
2014-10-30 22:41:54 +0100

    Shutdown on tmux environment value

--- scripts/sc.sh
+++ scripts/sc.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+DELAY=1;
+OUTPUT_FILE="$HOME/asd.log"
+
 FORMAT="\
 #{session_attached}:\
 #{window_active}:\
@@ -15,8 +18,48 @@ FORMAT="\
 #{pane_current_path}:\
 #{pane_pid}"
 
-while true;
-do
-sleep 1;
-tmux list-panes -a -F "$FORMAT" | grep "^1:1:1:" >> tmuxtime.log
-done
+getActivePane() {
+ tmux list-panes -a -F "$FORMAT" | grep "^1:1:1:" | cut -d: -f4-
+}
+
+record() {
+echo "record";
+  while true;
+  do
+
+    sleep "$DELAY";
+
+    isEndOfRecording
+    if [ $? -eq 0 ];
+      then break;
+    fi
+
+
+    echo "`getActivePane`" >> "$OUTPUT_FILE"
+
+  done
+}
+
+
+isEndOfRecording() {
+  END_FLAG=`tmux show-environment "end_rec" | grep '=true' | wc -l`
+
+  if [ $END_FLAG -eq 1 ];
+    then return 0;
+    else return 1;
+  fi
+}
+
+
+endRecording() {
+  tmux set-environment "end_rec" "true";
+  echo "ended"
+}
+
+
+startRecording() {
+  tmux set-environment "end_rec" "false";
+  record &
+  echo "started";
+}
+
