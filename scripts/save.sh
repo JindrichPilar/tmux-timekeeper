984db16e4bb38cc8f7ed73d3bcffa85978da41dd
Jindrich Pilar <pilar.jindrich@gmail.com>
2014-11-01 21:42:24 +0100

    save.sh dir structure description

---
+++ scripts/save.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+#Save data to file
+#Data structure must make possible statistics generation
+
+#Expected tree structure (make it dotfile? Encurage use of prepared scripts)
+
+#timekeeper_logs (dir)
+#session_name (dir per each session)
+#window_name (dir per each window in session)
+#current_path (dir per each pane in window)
+#current_command, pane_title (dirs for kinds of logs)      BEWARE: current_command and pane_title are redundant the time is stored duplicatly
+#time_spent.log (file per kind with)
+
+
+
