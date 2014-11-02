ed45b7f9d3ed5f28eb7a78cb4f3e32c975d426a1
Jindrich Pilar <pilar.jindrich@gmail.com>
2014-11-02 11:19:21 +0100

    Stats functions description

---
+++ stats/stats.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+
+#Stats functions
+#TODO implement functions
+#TODO BASH autocomplete
+
+
+#Get sessions with time spent
+
+
+
+#Get windows in session with time spent
+#1 = session_name
+
+
+
+#Get paths in session with time spent
+#1 = session_name
+
+
+
+#Get commands in session with time spent
+#1 = session_name
+
+
+
+#Get paths in window in session with time spent
+#1 = session_name
+#2 = window_name
+
+
+
+#Get commands in window in session with time spent
+#1 = session_name
+#2 = window_name
+
+
+
+#Get commands with time spent across all sessions and windows
+
+
+
+#Get paths with time spent accros all sessions and windows
